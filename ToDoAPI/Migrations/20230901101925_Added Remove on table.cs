27c0874de225383758532fde9c7f073c1a6d01f9
pc-1 <pc-1@PC>
2023-09-01 14:06:53 +0200

    Add project files.

---
+++ ToDoAPI/Migrations/20230901101925_Added Remove on table.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRemoveontable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRemoved",
+                table: "Todos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RemovedDate",
+                table: "Todos",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRemoved",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "RemovedDate",
+                table: "Todos");
+        }
+    }
+}
---
+++ ToDoAPI/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿namespace ToDoAPI.Models
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Note { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? DoneDate { get; set; }
+        public bool IsRemoved { get; set; }
+        public DateTime? RemovedDate { get; set; }
+
+    }
+}
